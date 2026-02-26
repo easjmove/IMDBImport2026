e9410e7302a157880a1f2fed916adb3b831d6057
move <move@zealand.dk>
2026-02-26 14:49:43 +0100

    Add project files.

---
+++ IMDBImport/NormalInserter.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBImport
+{
+    public class NormalInserter
+    {
+
+        public void InsertTitles(List<Title_Model> titles, SqlConnection sqlConn)
+        {
+            foreach (Title_Model movie in titles)
+            {
+                string query = "INSERT INTO Titles (" +
+                    "TConst, " +
+                    "TitleType, " +
+                    "PrimaryTitle, " +
+                    "OriginalTitle, " +
+                    "IsAdult, " +
+                    "StartYear, " +
+                    "EndYear, " +
+                    "RuntimeMinutes) " +
+                               "VALUES (" +
+                               movie.TConst + ", " +
+                               "'" + movie.TitleType + "', " +
+                               "'" + movie.PrimaryTitle.Replace("'", "''") + "', " +
+                               "'" + movie.OriginalTitle.Replace("'", "''") + "', " +
+                               "'" + movie.IsAdult + "', " +
+                               ConvertIntToString(movie.StartYear) + ", " +
+                               ConvertIntToString(movie.EndYear) + ", " +
+                               ConvertIntToString(movie.RuntimeMinutes) + ")";
+
+                try
+                {
+                    SqlCommand sqlComm = new SqlCommand(query, sqlConn);
+                    sqlComm.ExecuteNonQuery();
+                }
+                catch (SqlException sqlex)
+                {
+                    Console.WriteLine("Error inserting query:\r\n" + query);
+                }
+            }
+
+            
+        }
+        private string ConvertIntToString(int? value)
+        {
+            return value.HasValue ? value.Value.ToString() : "NULL";
+        }
+    }
+}
---
+++ IMDBImport/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using IMDBImport;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+Console.WriteLine("IMDB Import");
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+List<Title_Model> movies = new List<Title_Model>();
+
+foreach (string movie in File.ReadLines("C:/temp/title.basics.tsv").Skip(1).Take(10000))
+{
+    string[] parts = movie.Split('\t');
+    if (parts.Length == 9)
+    {
+        movies.Add(new Title_Model(parts));
+    }
+    else
+    {
+        Console.WriteLine("Invalid line: " + movie);
+    }
+}
+
+foreach (Title_Model movie in movies)
+{
+    Console.WriteLine(movie);
+}
+
+stopwatch.Stop();
+Console.WriteLine("Elapsed milliseconds to read from file: " + stopwatch.ElapsedMilliseconds);
+
+stopwatch.Start();
+
+NormalInserter inserter = new NormalInserter();
+SqlConnection sqlConn = new SqlConnection(
+    "Server=localhost;Database=IMDB;Integrated security=True;" +
+    "Trusted_Connection=True;TrustServerCertificate=True;");
+sqlConn.Open();
+inserter.InsertTitles(movies, sqlConn);
+
+sqlConn.Close();
+stopwatch.Stop(); // 4413 milconds for 10000 records, 4.4 seconds
+Console.WriteLine("Elapsed milliseconds to Insert Data: " + stopwatch.ElapsedMilliseconds);
